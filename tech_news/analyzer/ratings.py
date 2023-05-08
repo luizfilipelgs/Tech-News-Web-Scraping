44fe745a3dbab06ec0831f7d63fae2088767507a
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-08 19:58:05 -0300

    feat: add def top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,9 @@
-# Requisito 10
+from collections import Counter
+from tech_news.database import find_news
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    all_categories = [news["category"] for news in find_news()]
+    categories_count = Counter(all_categories)
+    top_categories = categories_count.most_common(5)
+    return [category[0] for category in top_categories]
