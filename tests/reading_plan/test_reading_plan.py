d363164afdda165957819d6b239a4ee0e405f17f
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-08 17:20:05 -0300

    feat: adiciona test_reading_plan_group_news

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,15 @@
 from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from tests.reading_plan.mock_news import mocked_news
+import pytest
+from unittest.mock import MagicMock
 
 
 def test_reading_plan_group_news():
-    pass
+    with pytest.raises(ValueError):
+        ReadingPlanService.group_news_for_available_time(-1)
+
+    ReadingPlanService._db_news_proxy = MagicMock(return_value=mocked_news)
+    res = ReadingPlanService.group_news_for_available_time(12)
+
+    assert len(res['readable']) == 2
+    assert len(res['unreadable']) == 1
