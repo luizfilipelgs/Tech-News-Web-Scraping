80cbee32d73e45fafee5532b444b1927ea4d20f9
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-08 19:51:13 -0300

    fix: test_reading_plan_group_news

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,15 +1,16 @@
 from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
-from tests.reading_plan.mock_news import mocked_news
 import pytest
 from unittest.mock import MagicMock
+from tests.reading_plan.mock_news import mock_data
 
 
 def test_reading_plan_group_news():
+    ReadingPlanService._db_news_proxy = MagicMock(return_value=mock_data)
+    response = ReadingPlanService.group_news_for_available_time(12)
+
+    assert len(response['readable']) == 2
+    assert len(response['unreadable']) == 1
+    assert response['readable'][0]['unfilled_time'] == 7
+
     with pytest.raises(ValueError):
         ReadingPlanService.group_news_for_available_time(-1)
-
-    ReadingPlanService._db_news_proxy = MagicMock(return_value=mocked_news)
-    res = ReadingPlanService.group_news_for_available_time(12)
-
-    assert len(res['readable']) == 2
-    assert len(res['unreadable']) == 1
