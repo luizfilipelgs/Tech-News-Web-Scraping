f26845ab3a54fc0a0d5769c472c2a2f734840be0
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-08 20:10:58 -0300

    fix: top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,9 +1,14 @@
 from collections import Counter
-from tech_news.database import find_news
+from tech_news.database import search_news
 
 
 def top_5_categories():
-    all_categories = [news["category"] for news in find_news()]
-    categories_count = Counter(all_categories)
-    top_categories = categories_count.most_common(5)
-    return [category[0] for category in top_categories]
+    all_news = search_news({})
+    all_categories = [news["category"] for news in all_news]
+    counted_categories = Counter(all_categories)
+    most_common_categories = counted_categories.most_common(5)
+    sorted_categories = sorted(
+        most_common_categories,
+        key=lambda category: (-category[1], category[0]),
+    )
+    return [category[0] for category in sorted_categories]
