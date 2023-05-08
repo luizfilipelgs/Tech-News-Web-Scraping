9c2bb23e1021216110bed06e7062c1b08eeaee1b
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-08 17:03:13 -0300

    fix: altera mock test

---
+++ tests/reading_plan/mock_news.py
@@ -0,0 +1,29 @@
+mock_data = [
+    {
+        "url": "https://blog.com/1",
+        "title": "UFF projeto Petrobras",
+        "timestamp": "01-05-2023",
+        "writer": "luiz",
+        "reading_time": 5,
+        "summary": "a UFF agega em seu time o dev luiz filipe como front-end para projetos com IA para petrobras",
+        "category": "Tech"
+    },
+    {
+        "url": "https://blog.com/2",
+        "title": "A experança para os dev retorna",
+        "timestamp": "02-05-2023",
+        "writer": "Filipe",
+        "reading_time": 10,
+        "summary": "A experança retorna no meio dev entre os junior",
+        "category": "Tech"
+    }
+    {
+        "url": "https://blog.com/3",
+        "title": "Empresas parceiras, são realmente parceiras ?",
+        "timestamp": "03-05-2023",
+        "writer": "Luiz Filipe",
+        "reading_time": 15,
+        "summary": "Empresa parceira reprova metade dos alunos da escola, em uma etapa que nem eliminatoria era, que parceria é essa ?",
+        "category": "Tech"
+    }
+]
