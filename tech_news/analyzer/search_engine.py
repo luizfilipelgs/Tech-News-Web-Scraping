3baa6a1661e4800b4cb64b37f755d8c3d1a2c6a9
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-08 18:45:00 -0300

    feat: add def search_by_title

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,6 +1,9 @@
-# Requisito 7
+from tech_news.database import search_news
+
+
 def search_by_title(title):
-    """Seu código deve vir aqui"""
+    query = {"title": {"$regex": title, "$options": "i"}}
+    return [(news["title"], news["url"]) for news in search_news(query)]
 
 
 # Requisito 8
