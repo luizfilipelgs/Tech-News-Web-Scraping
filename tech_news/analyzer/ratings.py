1c7cc91c0fc59eff2280ee12f5869ef7b6566bae
luizfilipelgs <luizfilipelgs@gmail.com>
2023-05-09 08:15:01 -0300

    refactor: altera def top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -6,9 +6,6 @@ def top_5_categories():
     all_news = search_news({})
     all_categories = [news["category"] for news in all_news]
     counted_categories = Counter(all_categories)
-    most_common_categories = counted_categories.most_common(5)
-    sorted_categories = sorted(
-        most_common_categories,
-        key=lambda category: (-category[1], category[0]),
-    )
-    return [category[0] for category in sorted_categories]
+    sorted_categories = sorted(counted_categories.items(), key=lambda x: (
+        -x[1], x[0]))
+    return [category for category, count in sorted_categories[:5]]
